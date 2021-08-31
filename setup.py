77ac9a3d1896bfbd54620507b6e69bd4632dd06d
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-08-31 07:06:14 +0200

    Renombre archivos y configure los setups

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='creatorClass',
+    version='0.0.1',
+    license='MIT',
+    packages=find_packages(),
+    description='Generador de clases en python',
+    long_description=open('README.md').read(),
+    author='Daniel Dubon Rodriguez',
+    author_email='danieldubon499@gmail.com',
+    url='https://github.com/DanielDubonDR/CreateClasses.git',
+    keywords=['class','object','createClass','generateClass','generatorClass'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
